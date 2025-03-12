88e322e197f73ed8b94be290072f99a61d21c5a7
Markus M. <markus-mi@web.de>
2025-03-12 07:54:17 +0100

    added the ability to view spool info by spool id

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def spool_info():
     return render_template('error.html', exception="MQTT is disconnected. Is the printer online?")
     
   try:
-    tag_id = request.args.get("tag_id")
-
+    tag_id = request.args.get("tag_id", "-1")
+    spool_id = request.args.get("spool_id", -1)
     last_ams_config = getLastAMSConfig()
     ams_data = last_ams_config.get("ams", [])
     vt_tray_data = last_ams_config.get("vt_tray", {})
@@ -106,11 +106,17 @@ def spool_info():
     spools = fetchSpools()
     current_spool = None
     for spool in spools:
+      if spool['id'] == spool_id:
+        current_spool = spool
+        break
+
       if not spool.get("extra", {}).get("tag"):
         continue
+
       tag = json.loads(spool["extra"]["tag"])
       if tag != tag_id:
         continue
+
       current_spool = spool
 
     # TODO: missing current_spool
@@ -130,8 +136,8 @@ def tray_load():
   tray_id = request.args.get("tray")
   spool_id = request.args.get("spool_id")
 
-  if not all([tag_id, ams_id, tray_id, spool_id]):
-    return render_template('error.html', exception="Missing RFID, AMS ID, or Tray ID or spool_id.")
+  if not all([ams_id, tray_id, spool_id]):
+    return render_template('error.html', exception="Missing AMS ID, or Tray ID or spool_id.")
 
   try:
     # Update Spoolman with the selected tray
