8854ccb0f723f6c129ca88966e8f71ac00dad0db
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-03-15 18:01:46 +0100

    Update tools_3mf.py

--- tools_3mf.py
+++ tools_3mf.py
@@ -120,7 +120,7 @@ def getMetaDataFrom3mf(url):
       elif url.startswith("local:"):
         download3mfFromLocalFilesystem(url.replace("local:", ""), temp_file)
       else:
-        download3mfFromFTP(url.replace("ftp://", "").replace(".gcode","").replace("_"," "), temp_file)
+        download3mfFromFTP(url.replace("ftp://", "").replace(".gcode",""), temp_file)
       
       temp_file.close()
 
