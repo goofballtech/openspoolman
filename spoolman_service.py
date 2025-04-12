a7236df2bd47831ae1c641a175f4ac7c6a04f267
Markus M. <markus-mi@web.de>
2025-04-12 14:27:17 +0200

    implemented costs of print
    The cost is calculated from the price of spool / filament from spoolman

--- spoolman_service.py
+++ spoolman_service.py
@@ -5,9 +5,43 @@ from zoneinfo import ZoneInfo
 from print_history import update_filament_spool
 import json
 
-from spoolman_client import consumeSpool, patchExtraTags, fetchSpoolList
+from spoolman_client import consumeSpool, patchExtraTags, fetchSpoolList, fetchSettings
 
 SPOOLS = {}
+SPOOLMAN_SETTINGS = {}
+
+currency_symbols = {
+    "AED": "د.إ", "AFN": "؋", "ALL": "Lek", "AMD": "դր.", "ANG": "ƒ", "AOA": "Kz", 
+    "ARS": "$", "AUD": "$", "AWG": "Afl.", "AZN": "₼", "BAM": "KM", "BBD": "$", 
+    "BDT": "৳", "BGN": "лв", "BHD": "د.ب", "BIF": "Fr", "BMD": "$", "BND": "$", 
+    "BOB": "$b", "BRL": "R$", "BSD": "$", "BTN": "Nu.", "BWP": "P", "BYN": "Br", 
+    "BZD": "$", "CAD": "$", "CDF": "Fr", "CHF": "CHF", "CLP": "$", "CNY": "¥", 
+    "COP": "$", "CRC": "₡", "CUP": "$", "CVE": "$", "CZK": "Kč", "DJF": "Fr", 
+    "DKK": "kr", "DOP": "RD$", "DZD": "دج", "EGP": "ج.م", "ERN": "Nfk", "ETB": "ታማ", 
+    "EUR": "€", "FJD": "$", "FKP": "£", "GBP": "£", "GEL": "₾", "GHS": "₵", 
+    "GIP": "£", "GMD": "D", "GNF": "Fr", "GTQ": "Q", "GYD": "$", "HKD": "$", 
+    "HNL": "L", "HRK": "kn", "HTG": "G", "HUF": "Ft", "IDR": "Rp", "ILS": "₪", 
+    "INR": "₹", "IQD": "د.ع", "IRR": "﷼", "ISK": "kr", "JMD": "$", "JOD": "د.أ", 
+    "JPY": "¥", "KES": "Sh", "KGS": "с", "KHR": "៛", "KMF": "Fr", "KRW": "₩", 
+    "KWD": "د.ك", "KYD": "$", "KZT": "₸", "LAK": "₭", "LBP": "ل.ل", "LKR": "Rs", 
+    "LRD": "$", "LSL": "L", "LTL": "Lt", "LVL": "Ls", "LYD": "د.ل", "MAD": "د.م.", 
+    "MDL": "lei", "MGA": "Ar", "MKD": "ден", "MMK": "K", "MNT": "₮", "MOP": "MOP", 
+    "MRO": "UM", "MRU": "MRU", "MUR": "Rs", "MVR": "Rf", "MWK": "MK", "MXN": "$", 
+    "MYR": "RM", "MZN": "MT", "NAD": "$", "NGN": "₦", "NIO": "C$", "NOK": "kr", 
+    "NPR": "₨", "NZD": "$", "OMR": "ر.ع.", "PAB": "B/.", "PEN": "S/", "PGK": "K", 
+    "PHP": "₱", "PKR": "₨", "PLN": "zł", "PYG": "₲", "QAR": "ر.ق", "RON": "lei", 
+    "RSD": "дин.", "RUB": "₽", "RWF": "Fr", "SAR": "ر.س", "SBD": "$", "SCR": "₨", 
+    "SEK": "kr", "SGD": "$", "SHP": "£", "SLL": "Le", "SOS": "Sh", "SRD": "$", 
+    "SSP": "£", "STD": "Db", "STN": "STN", "SYP": "ل.س", "SZL": "L", "THB": "฿", 
+    "TJS": "ЅМ", "TMT": "m", "TND": "د.ت", "TOP": "T$", "TRY": "₺", "TTD": "$", 
+    "TWD": "NT$", "TZS": "Sh", "UAH": "₴", "UGX": "Sh", "UYU": "$", "UZS": "лв", 
+    "VES": "Bs.S", "VND": "₫", "VUV": "Vt", "WST": "T", "XAF": "XAF", "XAG": "XAG", 
+    "XAU": "XAU", "XCD": "XCD", "XDR": "XDR", "XOF": "XOF", "XPF": "XPF", "YER": "ر.ي", 
+    "ZAR": "R", "ZMW": "ZK", "ZWL": "$"
+}
+
+def get_currency_symbol(code):
+    return currency_symbols.get(code, code)
 
 def trayUid(ams_id, tray_id):
   return f"{PRINTER_ID}_{ams_id}_{tray_id}"
@@ -121,7 +155,33 @@ def fetchSpools(cached=False):
     SPOOLS = fetchSpoolList()
     
     for spool in SPOOLS:
+      initial_weight = 0
+
+      if "initial_weight" in spool and spool["initial_weight"] > 0 :
+        initial_weight = spool["initial_weight"]
+      elif "weight" in spool["filament"] and spool["filament"]["weight"] > 0:
+        initial_weight = spool["filament"]["weight"]
+
+      price = 0
+      if "price" in spool and spool["price"] > 0:
+        price = spool["price"]
+      elif "price" in spool["filament"] and spool["filament"]["price"] > 0:
+        price = spool["filament"]["price"]
+
+      if initial_weight > 0 and price > 0:
+        spool["cost_per_gram"] = price / initial_weight
+      else:
+        spool["cost_per_gram"] = 0
+
       if "multi_color_hexes" in spool["filament"]:
         spool["filament"]["multi_color_hexes"] = spool["filament"]["multi_color_hexes"].split(',')
         
   return SPOOLS
+
+def getSettings(cached=False):
+  global SPOOLMAN_SETTINGS
+  if not cached or not SPOOLMAN_SETTINGS:
+    SPOOLMAN_SETTINGS = fetchSettings()
+    SPOOLMAN_SETTINGS['currency_symbol'] = get_currency_symbol(SPOOLMAN_SETTINGS["currency"])
+
+  return SPOOLMAN_SETTINGS
